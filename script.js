4ff3620ea3a85550d843501a796383946819c833
Saim Anis <saimdesign87@gmail.com>
2025-08-29 23:46:56 -0700

    Js Assignment 1 to 5

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//JavaScript Assignment chap 21 - 30
+let str = "Hello World!";
+console.log(str.length); 
+
+
+let stri = "Hello World!";
+console.log(stri.slice(6)); 
+
+
+let sent = "The cat is sleeping";
+if (sent.includes("cat")) {
+    console.log(true);
+} else{
+    console.log(false);
+}
+
+let catSent = "The cat is sleeping"
+
+console.log(catSent.indexOf("sleeping"))
+
+
+let text = "Javascript"
+ 
+ console.log(text[3])
+
+ 
