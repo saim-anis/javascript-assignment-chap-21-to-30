a6a95ee84b13b47e54464a4b8c259bc818cc4670
Saim Anis <saimdesign87@gmail.com>
2025-08-30 01:19:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -24,4 +24,49 @@ let text = "Javascript"
  
  console.log(text[3])
 
- 
+ let prog = "Programming"
+ console.log(prog[prog.length - 1])
+
+
+ let num = 7.8
+ let flr = Math.round(num)
+ console.log(flr)
+
+ let num2 = 5.9
+ let Num2 = Math.floor(num2)
+ console.log(Num2)
+
+
+ let num3 = 4.2
+ let Num3 = Math.ceil(num3)
+ console.log(Num3)
+
+//Generating Random Numbers
+ let Randnum = Math.random(1)
+ console.log(Randnum)
+
+let randomNum = Math.random() * 10 + 1;
+console.log(randomNum);
+
+
+ //Generating Random Numbers
+
+ let strno = 250
+ let Strno = strno.toString()
+ console.log(Strno)
+
+// . let strNum = "45.67"; Write a code to convert strNum to a number using 
+//Number() and print it. 
+let StrtoNo = "45.67"
+let Str = Number(StrtoNo)
+console.log(Str + 5)
+
+
+let numR = 5.6789;
+let rounded = numR.toFixed(2)
+console.log(rounded);
+
+let numLimit = 3.1415926535
+let No =  numLimit.toFixed(4) 
+
+console.log(No)
